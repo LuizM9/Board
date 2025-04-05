5660d07c06cce9efc2c7191148bcefe280b095b8
Luiz Carlos <luizcmarinho9@gmail.com>
2025-04-05 14:18:56 -0300

    Camada de acesso a dados

--- src/main/java/service/BoardService.java
+++ src/main/java/service/BoardService.java
@@ -1,7 +1,9 @@
 package service;
 
 import lombok.AllArgsConstructor;
+import persistence.dao.BoardColumnDAO;
 import persistence.dao.BoardDAO;
+import persistence.entity.BoardEntity;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -11,7 +13,27 @@ public class BoardService {
 
     private final Connection connection;
 
-    private boolean delete (final Long id) throws SQLException {
+    public BoardEntity insert(final BoardEntity entity) throws SQLException{
+        var dao = new BoardDAO(connection);
+        var boardColumnDAO = new BoardColumnDAO(connection);
+        try {
+            dao.insert(entity);
+            var columns = entity.getBoardColumns().stream().map(c -> {
+                c.setBoard(entity);
+                return c;
+            }).toList();
+            for (var column : columns){
+                boardColumnDAO.insert(column);
+            }
+            connection.commit();
+        }catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+        return entity;
+    }
+
+    public boolean delete (final Long id) throws SQLException {
         var dao = new BoardDAO(connection);
         try {
             if (!dao.exists(id)){
